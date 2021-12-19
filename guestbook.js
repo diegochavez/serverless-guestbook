a16fb950950b641765bfe4e4f2f8ee09ef3c3f49
Diego Chavez <di3gochav@gmail.com>
2021-12-19 21:01:10 +0100

    Add api url
    Nasty add URL should be an env var?

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://a78db6d0.eu-gb.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
